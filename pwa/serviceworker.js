f37b1bd43b38cd1793fcede40e21763683108612
idleboy <62786811+idleboy4038@users.noreply.github.com>
2022-05-13 17:25:49 +0530

    added pwa support.

---
+++ pwa/serviceworker.js
@@ -0,0 +1,32 @@
+
+// Change this to your repository name
+var GHPATH = '/';
+
+// Choose a different app prefix name
+var APP_PREFIX = 'pwa';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/pwa`,
+  `${GHPATH}/assets`,
+  `${GHPATH}/tabs`
+]
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDgcW1-9niOrkQyUFBzFD9aHUKLEG7cEpc",
+    authDomain: "idle-boy.firebaseapp.com",
+    projectId: "idle-boy",
+    storageBucket: "idle-boy.appspot.com",
+    messagingSenderId: "395887802821",
+    appId: "1:395887802821:web:31f5bc0b830af8d9abb3ac",
+    measurementId: "G-QRFNBZHPBT"
+  };
