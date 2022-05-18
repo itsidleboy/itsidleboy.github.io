0bb432af48b1fda5cc85aeb7a8fdf29023188cb3
idleboy <62786811+idleboy4038@users.noreply.github.com>
2022-05-18 13:04:55 +0530

    fix

--- sw.js
+++ sw.js
@@ -1,31 +1,3 @@
-var cacheName = '/';
-var filesToCache = [
-  './',
-  './index.html',
-  './ja',
-  './assets',
-  './tabs',
-  './_posts'
-];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
-
 var APP_PREFIX = 'Idle Boy'     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_1.5.2_Alpha'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
